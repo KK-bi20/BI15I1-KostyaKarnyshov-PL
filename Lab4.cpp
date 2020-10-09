8229a276be61368eafb2fe183bc98da48fa4e3ec
KK-bi20 <71899103+KK-bi20@users.noreply.github.com>
2020-10-09 19:16:30 +0600

    Add files via upload

---
+++ Lab4.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+
+int main() {
+    int array[] = { 1, 2, 3, 4, 5 };
+    int* begin = array;
+    int* end = array + sizeof(array) / sizeof(*array) - 1;
+
+    while (begin < end) {
+        *begin ^= *end ^= *begin ^= *end;
+        begin++; end--;
+    }
+
+    for (const int* p = array; p < array + sizeof(array) / sizeof(*array); p++) {
+        std::cout << *p << " ";
+    }
+}
